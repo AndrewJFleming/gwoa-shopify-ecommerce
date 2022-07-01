cb61d70b8eefeae52db04985210574a85d769232
Andrew Fleming <aflemi1@gmail.com>
2022-07-01 08:09:17 -0700

    Continue setting up shopContext
    -Manage checkout with componentDidMount.
    -Setup fetchAllProducts, createCheckout and fetchCheckout functions.
    -Wrap App component in ShopProvider.
    -

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -17,18 +17,39 @@ class ShopProvider extends React.Component {
     isMenuOpen: false,
   };
 
+  componentDidMount() {
+    if (localStorage.checkout_id) {
+      this.fetchCheckout(localStorage.checkout_id);
+    } else {
+      this.createCheckout();
+    }
+  }
+
   createCheckout = async () => {
+    const checkout = await client.checkout.create();
+    localStorage.setItem("checkout_id", checkout.id);
+    this.setState({ checkout: checkout });
   };
 
-  fetchCheckout = async () => {};
+  fetchCheckout = (checkoutId) => {
+    client.checkout.fetch(checkoutId).then((checkout) => {
+      this.setState({ checkout: checkout });
+    });
+  };
 
   addItemToCheckout = async () => {};
 
   removeLineItem = async (lineItemIdsToRemove) => {};
 
-  fetchAllProducts = async () => {};
+  fetchAllProducts = async () => {
+    const products = await client.product.fetchAll();
+    this.setState({ products: products });
+  };
 
-  fetchProductWithHandle = async (handle) => {};
+  fetchProductWithHandle = async (handle) => {
+    const product = await client.product.fetchByHandle(handle);
+    this.setState({ product: product });
+  };
 
   closeCart = () => {};
 
@@ -39,9 +60,27 @@ class ShopProvider extends React.Component {
   openMenu = () => {};
 
   render() {
-    return <ShopContext.Provider>{this.props.children}</ShopContext.Provider>;
+    return (
+      <ShopContext.Provider
+        value={{
+          ...this.state,
+          fetchPAllProducts: this.fetchAllProducts,
+          fetchProductWithHandle: this.fetchProductWithHandle,
+          addItemToCheckout: this.addItemToCheckout,
+          removeLineItem: this.removeLineItem,
+          closeCart: this.closeCart,
+          openCart: this.openCart,
+          closeMenu: this.closeMenu,
+          openMenu: this.openMenu
+        }}
+      >
+        {this.props.children}
+      </ShopContext.Provider>
+    );
   }
 }
+
+const ShopConsumer = ShopContext.Consumer;
 
 export { ShopConsumer, ShopContext };
 
