ce06fa7f086306ec302e1c7cb0fd4b39c9267717
Andrew Fleming <aflemi1@gmail.com>
2022-07-01 12:48:04 -0700

    Style display Home products with Grid and Card components
    -Force install @mui/styles
    -Use MUI Grid and Card components to display products on Home page.
    -Add logo image variant.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,16 @@
 import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
+import {
+    Grid,
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Typography,
+  } from "@mui/material";
+  import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
 import { ShopContext } from "../context/shopContext";
 
 const Home = () => {
@@ -12,11 +23,74 @@ const Home = () => {
 
   return (
     <div>
-      {products.map((product) => (
-        <Link to={`/products/${product.handle}`} key={product.title}>
-          {product.title}
-        </Link>
-      ))}
+      <Grid
+        container
+        spacing={3}
+        sx={{
+          width: "100%",
+          margin: "0px",
+        }}
+      >
+        {products.map((product) => (
+          <Grid item xs={12} sm={12} md={6} lg={3} key={product.id}>
+            <Card 
+            // sx={{ maxWidth: 345 }}
+            >
+              <Link
+                to={`/products/${product.handle}`}
+                // className={classes.productLink}
+              >
+                <CardMedia
+                  component="img"
+                  height="240"
+                  image={product.images[0].src}
+                  alt={"A preview of " + product.title}
+                />
+              </Link>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {product.title}
+                </Typography>
+                <Typography
+                  gutterBottom
+                  variant="body2"
+                  color="success.dark"
+                  component="div"
+                >
+                  ID: {product.id}
+                </Typography>
+                {/* <Typography variant="body2" gutterBottom>
+                  {product.desc.length <= 119
+                    ? product.desc
+                    : `${product.desc.substring(0, 120)}...`}
+                </Typography> */}
+                <Typography
+                  variant="subtitle1"
+                  gutterBottom
+                  sx={{
+                    justifySelf: "center",
+                    alignSelf: "center",
+                    textAlign: "center",
+                    margin: "20px 0px 30px 0px",
+                  }}
+                >
+                  {"Price:  $" + product.variants[0].price}
+                </Typography>
+
+                <CardActions>
+                  <Button
+                    size="small"
+                    onClick={() => {}}
+                    sx={{ color: "primary.main" }}
+                  >
+                    <ShoppingCartIcon /> <span>Add to Cart</span>
+                  </Button>
+                </CardActions>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
