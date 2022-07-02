b4b66b6e7f04a54a34c2ba16020b36019e0e636f
Andrew Fleming <aflemi1@gmail.com>
2022-07-02 10:31:28 -0700

    -Fix drawer button bug
    -Set menu button to display only on medium to large screens.
    -Add Home nav item.
    -Remove Login button.

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import {
   AppBar,
   Box,
@@ -12,11 +13,10 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 import { ShopContext } from "../../context/shopContext";
 
-const TopNav = ({handleDrawerToggle, drawerWidth}) => {
+const TopNav = ({ handleDrawerToggle, drawerWidth }) => {
   const { openCart, openMenu, checkout } = useContext(ShopContext);
   return (
-   
-          <AppBar
+    <AppBar
       position="fixed"
       sx={{
         width: { sm: `calc(100% - ${drawerWidth}px)` },
@@ -24,34 +24,34 @@ const TopNav = ({handleDrawerToggle, drawerWidth}) => {
         backgroundColor: "error.main",
       }}
     >
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerToggle}
+          sx={{ mr: 2, display: { sm: "none" } }}
+        >
+          <MenuIcon />
+        </IconButton>
 
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <ShoppingCartIcon />
-          </IconButton>
+        <Link to="/">
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Home
           </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    
+        </Link>
+        <IconButton
+          size="large"
+          // edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <ShoppingCartIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   );
 };
 
