aa371f91b38e82710c4c90c549c048c61b441b07
Andrew Fleming <aflemi1@gmail.com>
2022-07-02 10:24:39 -0700

    Setup basic ProductsPage
    -Use Grid to display product image and details side by side on all but mobile screens.

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,3 +1,13 @@
+import {
+  Grid,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardHeader,
+} from "@mui/material";
 import React, { useEffect, useContext } from "react";
 import { useParams, Link } from "react-router-dom";
 
@@ -13,7 +23,38 @@ const ProductPage = () => {
   }, [fetchProductWithHandle, handle]);
 
   if (!product.title) return <div>Loading...</div>;
-  return <div><h1>{product.title}</h1></div>;
+  return (
+    <div>
+      <Grid
+        container
+        spacing={3}
+        sx={{
+          width: "100%",
+          margin: "0px",
+        }}
+      >
+        <Grid item xs={12} lg={6} key={product.id}>
+          <img
+            style={{ width: "100%" }}
+            src={product.images[0].src}
+            alt={product.title}
+          />
+        </Grid>
+        <Grid item xs={12} lg={6} key={product.id}>
+          <Card>
+            <CardHeader title={product.title} />
+            <CardContent>
+              <Typography variant="h6">${product.variants[0].price}</Typography>
+              <Typography variant="body">{product.description}</Typography>
+            </CardContent>
+            <CardActions>
+              <Button onClick={() => addItemToCheckout(product.variants[0].id, 1)}>Add To Cart</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default ProductPage;
