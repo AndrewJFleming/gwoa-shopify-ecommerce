165ab34d4112e32200d300a346ec6cdf7f7210dc
Andrew Fleming <aflemi1@gmail.com>
2022-07-11 15:41:31 -0700

    Use makeStyles in TopNav and Footer
    -Add new GWoA logo to TopNav and Footer.
    -Use makeStyles in TopNav and Footer comps.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,67 +1,79 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import {  Box, Typography } from "@mui/material";
+import { Box, List, ListItem, ListItemText, Typography, Button } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+import transBGLogo from "../../assets/GWoA logo, alt.png";
+
+const useStyles = makeStyles((theme) => {
+  return {
+    footerWrapper: {
+      "& a": {
+        color: "#fff",
+        textDecoration: "underline",
+      },
+      backgroundColor: "#7e9c90",
+      height: "300px",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "space-between",
+      position: "absolute",
+      bottom: 0,
+      width: "100%",
+    },
+    footerTopWrapper: {
+      padding: "15px",
+      display: "flex",
+      justifyContent: "space-evenly",
+      alignItems: "center",
+      height: "100%",
+    },
+    footerBottomWrapper: {
+      backgroundColor: "rgba(0, 0, 0, 0.5)",
+    },
+  };
+});
 
 const Footer = () => {
+  const classes = useStyles();
   return (
-    <Box
-          component="div"
-          style={{
-            backgroundColor: "darkblue",
-            height: "300px",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            position: "absolute",
-            bottom: 0,
-            width: "100%",
-          }}
-        >
+    <Box className={classes.footerWrapper} component="div">
+      <Box component="div" className={classes.footerTopWrapper}>
+        <Box component="div" sx={{ display: { xs: "none", sm: "block" } }}>
           <Box
-            component="div"
-            style={{
-              padding: "15px",
-              display: "flex",
-              justifyContent: "space-evenly",
-              alignItems: "center",
-              height: "100%"
-            }}
-          >
-            <Box component="div" sx={{display: {xs: "none", sm: "block"}}}>
-              <img
-                style={{ maxHeight: "145px" }}
-                src="https://cdn.shopify.com/s/files/1/0472/5705/9496/files/Bath_Bomb_-_Product-5_-_trans_1.png?v=1610123549"
-              />
-            </Box>
-            <Box component="div" sx={{ textAlign: "center" }}>
-              <Typography variant="h5">Quick Links</Typography>
-              <ul style={{ paddingLeft: "0", listStyle: "none" }}>
-                <Link to="/">
-                  <li>Test</li>
-                </Link>
-                <Link to="/">
-                  <li>Test</li>
-                </Link>
-                <Link to="/">
-                  <li>Test</li>
-                </Link>
-              </ul>
-            </Box>
-          </Box>
-
-          <Box component="div" sx={{backgroundColor: "rgba(0, 0, 0, 0.5)"}}>
-            <Typography
-              textAlign="center"
-              color="white"
-              w="100%"
-              borderTop="1px solid white"
-              p="1rem"
-            >
-              © Copyright Great Wall of Art
-            </Typography>
-          </Box>
+            component="img"
+            sx={{ maxHeight: "145px", opacity: "0.5" }}
+            src={transBGLogo}
+          />
         </Box>
-  )
-}
+        <Box component="div" sx={{ textAlign: "center" }}>
+          <Typography variant="h5" color="common.white" sx={{opacity: "0.75"}}>Quick Links</Typography>
+          <Box component="ul" sx={{ paddingLeft: "0", listStyle: "none" }}>
+            <Link to="/">
+              <Box component="li">HOME</Box>
+            </Link>
+            <Link to="/">
+              <Box component="li">CART</Box>
+            </Link>
+          </Box>
+          <Button variant="contained">CHECKOUT</Button>
+        </Box>
+      </Box>
 
-export default Footer
+      <Box component="div" className={classes.footerBottomWrapper}>
+        <Typography
+          textAlign="center"
+          color="white"
+          w="100%"
+          borderTop="1px solid white"
+          p="1rem"
+          sx={{ opacity: "0.5" }}
+        >
+          © Copyright Great Wall of Art
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default Footer;
--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -10,19 +10,58 @@ import {
   Button,
   Box,
   Menu,
-  Container
+  Container,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
-import logoImage from './logo.png'
+import logoImage from "../../assets/GWoA logo, alt.png";
 
 import { ShopContext } from "../../context/shopContext";
 
+import { makeStyles } from "@mui/styles";
+
 const pages = [
-  {title: "Cart", path: "cart"}, 
-  {title: "About", path: "about"}, 
+  { title: "Cart", path: "cart" },
+  { title: "About", path: "about" },
 ];
+
+const useStyles = makeStyles((theme) => {
+  return {
+    headerLogo: {
+      flexGrow: 1,
+      display: "none",
+      [theme.breakpoints.down("md")]: {
+        display: "flex",
+      },
+      //Shared styles
+      textDecoration: "none",
+      mr: 2,
+      fontFamily: "PT Sans Narrow",
+      fontWeight: 700,
+      letterSpacing: ".3rem",
+      color: "inherit",
+      whiteSpace: "no-wrap",
+      fontSize: "1.5em",
+    },
+    //Style override for medium logo
+    headerLogoMd: {
+      flexGrow: 0,
+      display: "flex",
+      [theme.breakpoints.down("md")]: {
+        display: "none",
+      },
+    },
+    TopNavLinkItem: {
+      "& a": {
+        color: theme.palette.primary.main,
+      },
+      "& a:hover": {
+        textDecoration: "none",
+      },
+    },
+  };
+});
 
 const TopNav = ({ handleDrawerToggle, drawerWidth }) => {
   const { openCart, openMenu, checkout } = useContext(ShopContext);
@@ -45,167 +84,116 @@ const TopNav = ({ handleDrawerToggle, drawerWidth }) => {
     setAnchorElUser(null);
   };
 
+  const classes = useStyles();
+
   return (
-       <React.Fragment>
-    {/* <AppBar
-      position="fixed"
-      sx={{
-        width: { sm: `calc(100% - ${drawerWidth}px)` },
-        ml: { sm: `${drawerWidth}px` },
-        backgroundColor: "error.main",
-      }}
-    >
-      <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="open drawer"
-          onClick={handleDrawerToggle}
-          sx={{ mr: 2, display: { sm: "none" } }}
-        >
-          <MenuIcon />
-        </IconButton>
-
-        <Link to="/">
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Home
-          </Typography>
-        </Link>
-        <Link to="/cart">
-        <IconButton
-          size="large"
-          // edge="start"
-          color="inherit"
-          aria-label="menu"
-          // sx={{ mr: 2 }}
-        >
-          <ShoppingCartIcon/>
-        </IconButton>
-        </Link>
-          <span >({checkout.lineItems?.length})</span>
-      </Toolbar>
-    </AppBar> */}
-
-
-    <AppBar
-      position="fixed"
-      sx={{
-        width: { sm: `calc(100% - ${drawerWidth}px)` },
-        ml: { sm: `${drawerWidth}px` },
-        backgroundColor: "error.main",
-      }}
-    >
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-      <Link to="/">
-          <Box component="img" sx={{ width: "40px", display: { xs: 'none', md: 'flex' }, mr: 1 }} src={logoImage}/>
-    </Link>
-          <Typography
-            variant="h6"
-            noWrap
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            
+    <React.Fragment>
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+          backgroundColor: "error.main",
+        }}
+      >
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
             <Link to="/">
-            LOGO
-          </Link>
-          </Typography>
+              <Box
+                component="img"
+                sx={{
+                  width: "40px",
+                  display: { xs: "none", md: "flex" },
+                  mr: 1,
+                  opacity: "0.5",
+                }}
+                src={logoImage}
+              />
+            </Link>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleDrawerToggle}
-              color="inherit"
-              sx={{ mr: 2, display: { sm: "none" } }}
+            <Link
+              to="/"
+              className={`${classes.headerLogo} ${classes.headerLogoMd}`}
             >
-              <ShoppingCartIcon />
-            </IconButton>
-           
-          </Box>
-          <Link to="/">
-          <Box component="img" sx={{ width: "40px", display: { xs: 'flex', md: 'none' }, mr: 1 }} src={logoImage}/>
-          </Link>
-          <Typography
-            variant="h5"
-            noWrap
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-            >
-            <Link to="/">
-            LOGO
-              </Link>
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Link to={`/${page.path}`}>
-              <Button
-                key={page.title}
-                onClick={handleCloseNavMenu}
-                sx={{ color: 'white', display: 'block' }}
-                >
-                {page.title}
-              </Button>
-                </Link>
-            ))}
-          </Box>
+              Great Wall of Art
+            </Link>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Page Links">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0
-                }}>
-                <MenuIcon />
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleDrawerToggle}
+                color="inherit"
+                sx={{ mr: 2, display: { sm: "none" } }}
+              >
+                <ShoppingCartIcon />
               </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
+            </Box>
+            <Link to="/">
+              <Box
+                component="img"
+                sx={{
+                  width: "40px",
+                  display: { xs: "flex", md: "none" },
+                  mr: 1,
+                  opacity: "0.5",
+                }}
+                src={logoImage}
+              />
+            </Link>
+            <Link to="/" className={classes.headerLogo}>
+              Great Wall of Art
+            </Link>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }} className={classes.TopNavLinkItem}>
               {pages.map((page) => (
                 <Link to={`/${page.path}`}>
-                <MenuItem key={page.title} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{page.title}</Typography>
-                </MenuItem>
+                  <Button
+                    key={page.title}
+                    onClick={handleCloseNavMenu}
+                    sx={{ color: "white", display: "block" }}
+                  >
+                    {page.title}
+                  </Button>
                 </Link>
               ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
- 
+            </Box>
 
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Page Links">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <MenuIcon />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {pages.map((page) => (
+                  <Link to={`/${page.path}`}>
+                    <MenuItem key={page.title} onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">{page.title}</Typography>
+                    </MenuItem>
+                  </Link>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
     </React.Fragment>
   );
 };
